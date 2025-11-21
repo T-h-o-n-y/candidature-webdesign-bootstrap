3ac1f8f62f39d735c665ff5f8f674112526ae3e1
Anthony Sulewski <244550315+T-h-o-n-y@users.noreply.github.com>
2025-11-21 17:41:57 +0100

    feat : Adding Section Infinite & Awards

--- src/main.js
+++ src/main.js
@@ -2,4 +2,35 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../scss/main.scss';
 import * as bootstrap from 'bootstrap';
 
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Initialisation de Swiper
+  var swiper = new Swiper(".myAwardSwiper", {
+    slidesPerView: 1,      // Mobile : 1 carte visible
+    spaceBetween: 20,      // Espace entre les cartes
+    grabCursor: true,      // Curseur en forme de main
+    
+    // Configuration de la pagination (les points)
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+    // Points de rupture (Breakpoints) pour le responsive
+    breakpoints: {
+      // Quand l'écran est >= 768px (Tablette)
+      768: {             
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      // Quand l'écran est >= 992px (Desktop / LG)
+      992: {             
+        slidesPerView: 4, // On affiche 5 cartes comme sur ta grille d'origine
+        spaceBetween: 30,
+      },
+    },
+  });
+
+});
+
 
